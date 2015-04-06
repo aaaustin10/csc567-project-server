35561a7b31f5404215b18e710c97d255f7beff9f
Austin Stewart <aaaustin10@gmail.com>
2015-04-06 10:57:53 -0500

    Adds clipboard to installed apps and creates a simple model. Resolves #1

---
+++ clipboard/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('contents', models.CharField(max_length=1000)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- clipboard/models.py
+++ clipboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+class Entry(models.Model):
+    timestamp = models.DateTimeField(default=now)
+    contents = models.CharField(max_length=1000)
+    owner = models.ForeignKey(User)
