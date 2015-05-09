10ea2bdace15477c53ef9b357c0383d5415186e8
Austin Stewart <aaaustin10@gmail.com>
2015-05-09 17:15:50 -0500

    Change the web client to allow multiple users.

--- clipboard/static/js/clips.js
+++ clipboard/static/js/clips.js
@@ -1,23 +1,58 @@
+server_url = "http://localhost:8000/";
+
 function decode(data, key) {
     return CryptoJS.AES.decrypt(CryptoJS.lib.CipherParams.create({ciphertext: CryptoJS.enc.Hex.parse(data)}), CryptoJS.enc.Hex.parse(key), { mode: CryptoJS.mode.ECB }).toString(CryptoJS.enc.Utf8) 
 }
 
+owner_id = -1
 $(document).ready(function () {
-    function success_function(response) {
-        clips = JSON.parse(response)
-        for (var i = 0; i < clips.length; i++) {
-            clips[i].decoded_contents = decode(CryptoJS.enc.Base64.parse(clips[i].contents).toString(), '574d93e6298df2e83e5c6b4dc63ae9280eb04b7589aed4ec0e7bfa8e0bc27f80');
-            $('body').append('<p><button id=' + i +' class="clip">' + clips[i].decoded_contents + '</button></p>')
-        }
-        $(".clip").on('click', function() {
-            prompt('text', clips[parseInt(this.id)].decoded_contents);
-        })
+    function failure_function() {
+        Alert("There was an error communicating with the server.")
     }
 
-    var owner_id = 1;
-    $.ajax({
-        type: 'GET', 
-        url: 'http://localhost:8000/clipboard?owner_id=' + owner_id,
-        success: success_function,
+    function get_and_display_clips() {
+        function success_function(response) {
+            $('.clip').remove();
+            clips = JSON.parse(response)
+            var password = $("#password").val()
+            for (var i = 0; i < clips.length; i++) {
+                clips[i].decoded_contents = decode(CryptoJS.enc.Base64.parse(clips[i].contents).toString(), CryptoJS.SHA256(password).toString());
+                $('body').append('<p><button id=' + i +' class="clip">' + clips[i].decoded_contents + '</button></p>')
+            }
+            $(".clip").on('click', function() {
+                prompt('text', clips[parseInt(this.id)].decoded_contents);
+            })
+        }
+
+        $.ajax({
+            type: 'GET', 
+            url: server_url + 'clipboard?owner_id=' + owner_id,
+            success: success_function,
+            error: failure_function,
+        });
+    }
+
+    function fetch(response) {
+        $('#login-form').hide();
+        owner_id = response;
+        $('#fetch').show();
+        get_and_display_clips();
+    }
+
+    $('#get-button').click(function() {
+        var username = $("#username").val()
+        var password = $("#password").val()
+        var passkey = CryptoJS.SHA256(username + password);
+        console.log(passkey);
+
+        $.ajax({
+            type: 'POST',
+            url: server_url + 'login',
+            success: fetch,
+            data: JSON.stringify({username: username, passkey: passkey.toString()}),
+        });
     });
+
+    $('#fetch').click(get_and_display_clips);
+    $('#fetch').hide();
 });
